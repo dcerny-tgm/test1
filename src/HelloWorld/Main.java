de947d50dfe1c0e19235a3f655af218368df0736
dcerny <dcerny@student.tgm.ac.at>
2018-01-30 11:39:55 +0100

    Test1

--- src/HelloWorld/Main.java
+++ src/HelloWorld/Main.java
@@ -1,8 +1,12 @@
 package HelloWorld;
 
 public class Main {
-
 	public static void main(String[] args){
+		HelloWorld w = new HelloWorld();
+		w.HelloWorld();
+		w.HelloWorld();
+		w.HelloWorld();
+		w.HelloWorld();
+		w.HelloWorld();
 	}
-
 }
